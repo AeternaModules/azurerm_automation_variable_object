669ece31aaa02f3cfceb01eddad2fc5321ee3a6f
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:56:52 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "automation_variable_objects" {
+  description = "All automation_variable_object resources"
+  value       = azurerm_automation_variable_object.automation_variable_objects
+}
+
